c851da26279d6dc71fbe56ebd48eb8cd79172e2b
emi~ <esauvisky@gmail.com>
2023-03-05 12:54:07 -0300

    great ver

--- main.py
+++ main.py
@@ -3,19 +3,16 @@ import os
 import sys
 
 import numpy as np
-from loguru import logger
-import numpy as np
-import tensorflow as tf
-from keras.layers import LSTM, Conv2D, Dense, Flatten, MaxPooling2D, Reshape, ConvLSTM2D, MaxPooling3D, TimeDistributed, Dropout
-from keras.models import Sequential
 from keras.callbacks import EarlyStopping
+from keras.layers import (ConvLSTM2D, Dense, Dropout, Flatten, MaxPooling3D, TimeDistributed)
+from keras.models import Sequential
 from keras.preprocessing.image import image_utils
-from tensorflow import keras
+from loguru import logger
 from sklearn.model_selection import train_test_split
 
-SEQUENCE_LENGTH = 10
-IMAGE_HEIGHT = 480 // 10
-IMAGE_WIDTH = 640 // 10
+SEQUENCE_LENGTH = 15
+IMAGE_HEIGHT = 480 // 5
+IMAGE_WIDTH = 640 // 5
 EPOCHS = 10
 FILENAME = f'weights-{SEQUENCE_LENGTH}_{IMAGE_HEIGHT}_{IMAGE_WIDTH}.h5'
 
@@ -72,7 +69,6 @@ def create_convlstm_model():
     #     ))
     # model.add(MaxPooling3D(pool_size=(1, 2, 2)))
 
-
     model.add(Flatten())
     # model.add(Dense(128, activation='relu'))
     model.add(Dense(1, activation='sigmoid'))
@@ -112,35 +108,38 @@ if __name__ == "__main__":
     setup_logging("INFO")
 
     X, y = load_data("./data/")
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, shuffle=True)
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.5, shuffle=True)
 
     convlstm_model = create_convlstm_model()
     convlstm_model.compile(loss='binary_crossentropy', optimizer='Adam', metrics=["accuracy"])
 
     # train
-    callback = EarlyStopping(monitor='val_accuracy', patience=5, min_delta=0.1)
-    convlstm_model_training_history = convlstm_model.fit(
-        x=X_train,
-        y=y_train,
-        epochs=EPOCHS,
-        batch_size=4,
-        shuffle=True,
-        validation_data=(X_test, y_test),
-        callbacks=[callback],
-    )
-    convlstm_model.save_weights(FILENAME)
+
+    if not os.path.exists(FILENAME):
+        convlstm_model_training_history = convlstm_model.fit(
+            x=X_train,
+            y=y_train,
+            epochs=EPOCHS,
+            batch_size=8,
+            validation_data=(X_test, y_test),
+            callbacks=[EarlyStopping(monitor='val_accuracy', patience=5, min_delta=0.02)],
+        )
+        convlstm_model.save_weights(FILENAME)
 
     # predict the test set
     convlstm_model.load_weights(FILENAME)
-    convlstm_model_predictions = convlstm_model.predict(X_test)
+    convlstm_model_predictions = convlstm_model.predict(X)
     convlstm_model_predictions = (convlstm_model_predictions > 0.5).astype(int)
 
     # check and show results
+    last_result = 0
     for i in range(len(convlstm_model_predictions)):
-        if convlstm_model_predictions[i] != y_test[i]:
-            logger.warning(f"This was predicted {'sleep' if convlstm_model_predictions[i] else 'not sleep'} and is actually {'sleep' if y_test[i] else 'not sleep'}")
-            image = image_utils.array_to_img(X_test[i][0])
-            image.show()
-            input()
+        if convlstm_model_predictions[i] != y[i]:
+            logger.warning(f"This was predicted {'sleep' if convlstm_model_predictions[i] else 'not sleep'} and is actually {'sleep' if y[i] else 'not sleep'}")
         else:
             logger.success(f"{i+1}.jpg was predicted {'sleep' if convlstm_model_predictions[i] else 'not sleep'}!")
+        if last_result != y[i]:
+            image = image_utils.array_to_img(X[i][0])
+            image.show()
+            input()
+            last_result = y[i]
