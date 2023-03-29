43d8bac6fdb75a52e8c956fbb19494ab1af9003c
emi~ <esauvisky@gmail.com>
2023-03-29 07:17:07 -0300

    refactor data loading

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 import datetime
+from math import ceil
 import os
 import random
 import shlex
@@ -31,7 +32,7 @@ import wandb
 SEQUENCE_LENGTH = 16
 IMAGE_HEIGHT = 480 // 2
 IMAGE_WIDTH = 640 // 2
-BATCH_SIZE = 10
+BATCH_SIZE = 32
 EPOCHS = 50
 LEARNING_RATE = 1e-4
 PATIENCE = 5
@@ -56,7 +57,6 @@ def create_model(input_shape):
     inputs = Input(shape=input_shape)
     x = ConvLSTM2D(filters=2, kernel_size=(5, 5), activation="tanh", recurrent_dropout=0.2, return_sequences=True)(inputs)
     x = MaxPooling3D(pool_size=(2, 2, 2))(x)
-    # x = TimeDistributed(GlobalAveragePooling2D())(x)
     x = Flatten()(x)
     x = Dense(4, activation="relu")(x)
     outputs = Dense(1, activation="sigmoid")(x)
@@ -90,14 +90,14 @@ class CustomBatchEndCallback(Callback):
             images = []
             for sequence_ix, _img in enumerate(X_step):
                 # image is the same as self.x_train[batch_ix * BATCH_SIZE + ix][0]
-                img = wandb.Image(_img, caption=f"Image {batch_ix*BATCH_SIZE + sequence_ix} - Label: {y_step}")
+                img = wandb.Image(_img, caption=f"Image {batch_ix * BATCH_SIZE + sequence_ix} - Label: {y_step}")
                 images.append(img)
 
             # Adds the row to the table with the actual index of the first image of the sequence,
             # the original label y, and the images
             date = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
             self.test_table.add_data(batch_ix * BATCH_SIZE, date, y_step, *images)
-            print(f" | Samples {batch_ix*BATCH_SIZE}-{batch_ix*BATCH_SIZE + SEQUENCE_LENGTH - 1} - Label: {y_step})'")
+            print(f" | Samples {batch_ix * BATCH_SIZE}-{batch_ix * BATCH_SIZE + SEQUENCE_LENGTH - 1} - Label: {y_step})'")
 
     def on_epoch_end(self, epoch, logs=None):
         wandb.log({"data": self.test_table}, commit=True)
@@ -139,9 +139,6 @@ def get_image(image_path, image_height, image_width):
 def process_image_sequence(image_files, images_path, image_height, image_width):
     images = []
     labels = []
-    augmented_images = []
-    datagen = ImageDataGenerator(rotation_range=10)
-    seed = random.randint(0, 1000)
 
     for file in image_files:
         image = get_image(os.path.join(images_path, file), image_height, image_width)
@@ -149,17 +146,11 @@ def process_image_sequence(image_files, images_path, image_height, image_width):
         images.append(image)
         labels.append(label)
 
-        # Augmentation
-        transformed = datagen.random_transform(image, seed=seed)
-        cropped = get_random_crop(transformed, seed=seed)
-        augmented_images.append(cropped)
-
-    return images, labels[-1], augmented_images
+    return images, labels[-1]
 
 
 def load_data(images_path, seq_length, image_height, image_width):
     X, y = [], []
-    X_aug, y_aug = [], []
 
     # Get the list of image files
     image_files = sorted((file for file in os.listdir(images_path) if file.endswith(".jpg")),
@@ -168,9 +159,11 @@ def load_data(images_path, seq_length, image_height, image_width):
     # Use a ThreadPoolExecutor to process image sequences in parallel
     with ThreadPoolExecutor(max_workers=16) as executor:
         # Create the tqdm progress bar
-        progress_bar = tqdm(total=len(image_files) - seq_length,
-                            smoothing=0.1,
-                            desc="Processing images...",)
+        progress_bar = tqdm(
+            total=len(image_files) - seq_length,
+            smoothing=0.1,
+            desc="Processing images...",
+        )
 
         # Submit the tasks to the executor
         futures = [
@@ -180,57 +173,52 @@ def load_data(images_path, seq_length, image_height, image_width):
 
         # Use as_completed() to process the results as they become available, and update the progress bar
         for future in as_completed(futures):
-            images, label, augmented_images = future.result()
+            images, label = future.result()
             X.append(images)
-            X_aug.append(augmented_images)
             y.append(label)
-            y_aug.append(label)
             progress_bar.update(1)
 
         progress_bar.close()
 
-    logger.info("Data loaded. Preparing arrays...")
+    logger.info("Data loaded.")
     X = np.array(X)
     y = np.array(y)
-    X_aug = np.array(X_aug)
-    y_aug = np.array(y_aug)
-    X = np.concatenate((X, X_aug), axis=0)
-    y = np.concatenate((y, y_aug), axis=0)
     return X, y
-
-
-def prepare_data():
-    if os.path.exists(".data"):
-        logger.info("Loading data from disk...")
-        X, y = np.load(".data/data.npz")["arr_0"], np.load(".data/data.npz")["arr_1"]
-    else:
-        X, y = load_data("./data", SEQUENCE_LENGTH, IMAGE_HEIGHT, IMAGE_WIDTH)
-        os.makedirs(".data", exist_ok=True)
-        logger.info("Saving data to disk...")
-        np.savez(".data/data.npz", X, y)
-    indices = np.random.permutation(len(X)).tolist()
-    X_train, y_train = X[indices], y[indices]
-    X_val, y_val = X[indices], y[indices]
-    X_train = X_train[:int(len(X) * 0.8)]
-    y_train = y_train[:int(len(y) * 0.8)]
-    X_val = X_val[int(len(X) * 0.8):]
-    y_val = y_val[int(len(y) * 0.8):]
-    X = np.array(X, dtype=np.float16)
-    y = np.array(y, dtype=np.int8)
-    return {"X": X, "y": y, "X_train": X_train, "y_train": y_train, "X_val": X_val, "y_val": y_val}
 
 
 def data_generator(X, y, batch_size):
     num_samples = len(X)
+    datagen = ImageDataGenerator(rotation_range=10)
+
+    half_batch_size = ceil(batch_size / 2)
     while True:
         # Generate batches of data
-        for start in range(0, num_samples, batch_size):
-            if start + batch_size > num_samples:
+        for start in range(0, num_samples, half_batch_size):
+            if start + half_batch_size > num_samples:
                 break
-            end = start + batch_size
+            end = start + half_batch_size
             X_batch = X[start:end]
             y_batch = y[start:end]
-            np.divide(X_batch, 255.0, out=X_batch)
+            seed = random.randint(0, 1000)
+
+            # Data augmentation
+            X_batch_aug = []
+            for c, image_sequence in enumerate(X_batch):
+                if c >= batch_size - half_batch_size: break
+                augmented_sequence = []
+                for image in image_sequence:
+                    transformed = datagen.random_transform(image, seed=seed)
+                    cropped = get_random_crop(transformed, seed=seed)
+                    augmented_sequence.append(cropped)
+                X_batch_aug.append(augmented_sequence)
+
+            X_batch_aug = np.array(X_batch_aug)
+            np.divide(X_batch, 255.0, out=X_batch, casting="unsafe")
+            np.divide(X_batch_aug, 255.0, out=X_batch_aug, casting="unsafe")
+            X_batch = np.concatenate((X_batch, X_batch_aug), axis=0)
+            y_batch = np.concatenate((y_batch, y_batch), axis=0)
+            X_batch = X_batch.astype(np.float16)
+            y_batch = y_batch.astype(np.int8)
             yield X_batch, y_batch
 
 
@@ -238,12 +226,16 @@ def set_mixed_precision():
     # Enable mixed precision training
     policy = mixed_precision.Policy('mixed_float16')
     mixed_precision.set_global_policy(policy)
-    # # Wrap the input pipeline with a Cast layer
-    # if policy.name == 'mixed_float16':
-    #     cast_dtype = 'float16'
-    # else:
-    #     cast_dtype = 'float32'
-    # return cast_dtype
+
+
+def get_generator(generator, X, y):
+    return tf.data.Dataset.from_generator(generator=generator,
+                                          args=(X, y, BATCH_SIZE),
+                                          output_types=(tf.float16, tf.int8),
+                                          output_shapes=(
+                                              (BATCH_SIZE, SEQUENCE_LENGTH, IMAGE_HEIGHT, IMAGE_WIDTH, 1),
+                                              (BATCH_SIZE,),
+                                          )).prefetch(tf.data.experimental.AUTOTUNE)
 
 
 if len(sys.argv) >= 2 and sys.argv[1] == "train":
@@ -265,9 +257,16 @@ wandb.init(project=f"aweful-{routine}",
                "batch_size": BATCH_SIZE,})
 
 set_mixed_precision()
-d = prepare_data()
-X, y, X_train, y_train = d["X"], d["y"], d["X_train"], d["y_train"]
-X_val, y_val = d["X_val"], d["y_val"]
+X, y = load_data("./data", SEQUENCE_LENGTH, IMAGE_HEIGHT, IMAGE_WIDTH)
+
+# Split the data
+indices = np.random.permutation(len(X)).tolist()
+X_train, y_train = X[indices], y[indices]
+X_val, y_val = X[indices], y[indices]
+X_train = X_train[:int(len(X) * 0.8)]
+y_train = y_train[:int(len(y) * 0.8)]
+X_val = X_val[int(len(X) * 0.8):]
+y_val = y_val[int(len(y) * 0.8):]
 
 # Create the ConvLSTM model
 input_shape = (SEQUENCE_LENGTH, IMAGE_HEIGHT, IMAGE_WIDTH, 1)
@@ -277,27 +276,14 @@ model = create_model(input_shape)
 optimizer = Adam(learning_rate=LEARNING_RATE)
 model.compile(loss="binary_crossentropy", optimizer=optimizer, metrics=["accuracy"])
 
-if os.path.exists(FILENAME):
-    model.load_weights(FILENAME)
-    logger.success(f"Loaded model weights from {FILENAME}")
+# if os.path.exists(FILENAME):
+#     model.load_weights(FILENAME)
+#     logger.success(f"Loaded model weights from {FILENAME}")
 
 if routine == "train":
     with tf.device("CPU"):
-        train_dataset = tf.data.Dataset.from_generator(generator=data_generator,
-                                                       args=(X_train, y_train, BATCH_SIZE),
-                                                       output_types=(tf.float16, tf.int8),
-                                                       output_shapes=(
-                                                           (BATCH_SIZE, SEQUENCE_LENGTH, IMAGE_HEIGHT, IMAGE_WIDTH, 1),
-                                                           (BATCH_SIZE,),
-                                                       )).prefetch(tf.data.experimental.AUTOTUNE)
-
-        val_dataset = tf.data.Dataset.from_generator(generator=data_generator,
-                                                     args=(X_val, y_val, BATCH_SIZE),
-                                                     output_types=(tf.float16, tf.int8),
-                                                     output_shapes=(
-                                                         (BATCH_SIZE, SEQUENCE_LENGTH, IMAGE_HEIGHT, IMAGE_WIDTH, 1),
-                                                         (BATCH_SIZE,),
-                                                     )).prefetch(tf.data.experimental.AUTOTUNE)
+        train_dataset = get_generator(data_generator, X_train, y_train)
+        val_dataset = get_generator(data_generator, X_val, y_val)
 
     # Compute the number of steps per epoch
     steps_per_epoch = len(X_train) // BATCH_SIZE
@@ -329,23 +315,13 @@ if routine == "train":
     # Save the model weights
     model.save_weights(FILENAME)
 
-    # # Make predictions on the test set
+    # Make predictions on the test set
     y_pred = model.predict(X_val)
     y_pred_classes = np.round(y_pred)
 
-    # # Evaluate the model performance
+    # Evaluate the model performance
     logger.info("\nConfusion matrix:\n" + str(confusion_matrix(y_val, y_pred_classes)))
     logger.info("\nClassification report:\n" + str(classification_report(y_val, y_pred_classes)))
-
-    # # Predict on the entire dataset to look for patterns
-    # with tf.device("CPU"):
-    #     X_predict = tf.data.Dataset.from_generator(generator=data_generator,
-    #                                                args=(X, y, BATCH_SIZE),
-    #                                                output_types=(tf.float16, tf.int8),
-    #                                                output_shapes=(
-    #                                                    (BATCH_SIZE, SEQUENCE_LENGTH, IMAGE, IMAGE_WIDTH, 1),
-    #                                                    (BATCH_SIZE,),
-    #                                                )).prefetch(tf.data.experimental.AUTOTUNE)
 
 # for i in range(0, len(X), BATCH_SIZE):
 #     X_predict = X[i:i + BATCH_SIZE]
@@ -361,20 +337,17 @@ if routine == "train":
 if routine == "clock":
     sleep_counter = deque(maxlen=6 * 60)
     images = deque(maxlen=SEQUENCE_LENGTH)
-    # images_batch = deque(maxlen=BATCH_SIZE)
     path = "/tmp/image.jpg"
 
     main_table = create_wandb_table()
     index = 0
     while True:
         index += 1
-        # Take a photo
         subprocess.run(shlex.split(f"fswebcam {path} -d /dev/video0 -S2 -F1"),
                        check=False,
                        stdout=subprocess.DEVNULL,
                        stderr=subprocess.DEVNULL)
 
-        # Load and add the preprocessed image to the sequence
         image = image_utils.load_img(path, color_mode="grayscale")
         image = image_utils.img_to_array(image) / 255.0
         image = image[:-21, :]
@@ -382,7 +355,6 @@ if routine == "clock":
         resized = image_utils.img_to_array(resized, dtype=np.float16) / 255.0
         images.append(resized)
 
-        # Start making predictions only after the sequence is full
         if len(images) < SEQUENCE_LENGTH:
             continue
 
@@ -390,7 +362,6 @@ if routine == "clock":
         y_loop = model.predict(X_loop, verbose=0)
         y_loop_class = np.round(y_loop).flatten().astype(int)[0]
 
-        # Send data to wandb
         date = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
         prediction = "Awake 🆙" if y_loop_class == 0 else "Sleep 💤"
         main_table.add_data(index, date, prediction, *[wandb.Image(_img) for _img in images])
@@ -408,10 +379,8 @@ if routine == "clock":
             sleep_counter.append(0)
             logger.info("No sleep detected")
 
-        # 80% of the last 6 hours
         if sleep_counter.count(1) > 0.8 * 6 * 60:
             print("Wake up! You've been sleeping for more than 6 hours!")
-            # Implement your alarm triggering mechanism here
             alarm_triggered = True
         else:
             alarm_triggered = False
