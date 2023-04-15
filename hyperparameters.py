5969251ea5d1c0a58ccf267b01d456fb78dd1c4a
emi~ <esauvisky@gmail.com>
2023-04-15 20:22:48 -0300

    feat: add comments to hyperparameters and model architecture

--- hyperparameters.py
+++ hyperparameters.py
@@ -1,9 +1,14 @@
-# Define default hyperparameters
+# These are linked to the dataset.
+# You cannot change them without deleting
+# and regenerating the entire dataset.
 SEQUENCE_LENGTH = 8
-BATCH_SIZE = 32
 IMAGE_HEIGHT = 480 // 2
 IMAGE_WIDTH = 640 // 2
 
+# These are linked to the model architecture.
+# You can change them experimentally, as long
+# as the model does not change or you retrain it.
+BATCH_SIZE = 32
 EPOCHS = 50
 LEARNING_RATE = 1e-4
 PATIENCE = 10
